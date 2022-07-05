35a29a87ba8040b4439489065309056569e4fd45
mac <eniworoeva@yahoo.com>
2022-07-05 14:45:01 +0100

    input

--- main.go
+++ main.go
@@ -11,8 +11,15 @@ func main()  {
 	fmt.Printf("we have a total of %v tickets but %v left.\n",trainTickets, remainingTickets )
 	fmt.Println("Please book your tickets")
 
-	var useName string
+	var userName string
 	var userTickets uint
+
+	fmt.Println("Please enter your name:")
+	fmt.Scan(&userName) 
+	fmt.Println("how many tickets would you like to purchase?")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("%v has booked %v tickets.\n",userName, userTickets )
 
 	 
 
