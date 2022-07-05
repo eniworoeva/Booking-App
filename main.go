4dd6dd856aac52172591b8e1bf7ff16259e2024c
mac <eniworoeva@yahoo.com>
2022-07-05 14:24:18 +0100

    daily input

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var stationName string = "Oreva metro station"
+	const trainTickets int = 50
+	var remainingTickets int = 50
+
+	fmt.Printf("Welcome to %v.\n", stationName)
+	fmt.Printf("we have a total of %v tickets but %v left.\n",trainTickets, remainingTickets )
+	fmt.Println("Please book your tickets")
+
+	var useName string
+	var userTickets uint
+
+	 
+
+
+
+
+
+}
