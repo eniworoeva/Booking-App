2430258176cffb4fb89a8e32f10851cde0512787
mac <eniworoeva@yahoo.com>
2022-07-10 06:46:17 +0100

    input

--- main.go
+++ main.go
@@ -5,47 +5,36 @@ import (
 	"strings"
 )
 
+var stationName string = "Oreva metro station"
+
+const trainTickets int = 50
+
+var remainingTickets int = 50
+var bookings []string
+
 func main() {
-	var stationName string = "Oreva metro station"
-	const trainTickets int = 50
-	var remainingTickets int = 50
-	var bookings []string
 
-	greetUser(stationName, trainTickets, remainingTickets)
-
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	greetUser()
 
 	for {
 
-		fmt.Println("Please enter your first name:")
-		fmt.Scan(&firstName)
-		fmt.Println("Please enter your last name:")
-		fmt.Scan(&lastName)
-		fmt.Println("Please enter your email:")
-		fmt.Scan(&email)
-		fmt.Println("how many tickets would you like to purchase?")
-		fmt.Scan(&userTickets)
+		//calls getUserInput function
+		firstName, lastName, email, userTickets := getUserInput()
 
-		isValidEmail, isValidNames, isValidTickets := validateUserInput(firstName, lastName, email, userTickets, uint(remainingTickets))
+		isValidEmail, isValidNames, isValidTickets := validateUserInput(firstName, lastName, email, userTickets)
 
 		//checks to make sure the user doesn't input a number > than tickets remaining
 		if isValidTickets && isValidEmail && isValidNames {
 
-			remainingTickets = remainingTickets - int(userTickets)
-			bookings = append(bookings, firstName+" "+lastName)
-
-			fmt.Printf("Thank you %v %v for purchasing %v tickets, you will recieve a confirmation email at %v shortly \n", firstName, lastName, userTickets, email)
-			fmt.Printf("%v tickets are left\n", remainingTickets)
+			bookingLogic(userTickets, firstName, lastName, email)
 
 			//call first name function
-			firstNames := userFirstName(bookings)
+			firstNames := userFirstName()
 			fmt.Printf("The first names of the bookings are:%v\n", firstNames)
 
 			//checks if tickects are still available
 			if remainingTickets == 0 {
+				//end program
 				fmt.Println("Train tickets are booked out.  ")
 				break
 			}
@@ -66,13 +55,13 @@ func main() {
 
 }
 
-func greetUser(stationName string, trainTickets int, remainingTickets int) {
+func greetUser() {
 	fmt.Printf("welcome to %v ", stationName)
 	fmt.Printf("we have a total of %v tickets but %v left.\n", trainTickets, remainingTickets)
 	fmt.Println("Please book your tickets")
 
 }
-func userFirstName(bookings []string) []string {
+func userFirstName() []string {
 	firstNames := []string{}
 	//loop through the values and ignore the indices
 	for _, val := range bookings {
@@ -83,9 +72,35 @@ func userFirstName(bookings []string) []string {
 	}
 	return firstNames
 }
-func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
 	isValidNames := len(firstName) >= 2 && len(lastName) >= 2
 	isValidEmail := strings.Contains(email, "@")
 	isValidTickets := userTickets > 0 && userTickets <= uint(remainingTickets)
 	return isValidEmail, isValidNames, isValidTickets
+}
+func getUserInput() (string, string, string, uint) {
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Please enter your first name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Please enter your email:")
+	fmt.Scan(&email)
+	fmt.Println("how many tickets would you like to purchase?")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+func bookingLogic(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - int(userTickets)
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for purchasing %v tickets, you will recieve a confirmation email at %v shortly \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets are left\n", remainingTickets)
+
 }
